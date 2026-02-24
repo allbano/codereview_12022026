88ad23c8e1f1ce79b9f0515bd41c1acab7932178
DanSimba <danteksimba@gmail.com>
2026-02-24 20:01:59 -0300

    feat: adiciona o componente shared/components/modal-confirm

---
+++ frontend/src/app/shared/components/modal-confirm/modal-confirm.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-modal-confirm',
+  imports: [],
+  templateUrl: './modal-confirm.html',
+  styleUrl: './modal-confirm.css',
+})
+export class ModalConfirm {
+
+}
